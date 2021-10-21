ee09acea2d5a048e74ba60ba3dda96d9a320d5c4
Emilis Panovas <disordered@users.noreply.github.com>
2021-10-21 09:32:03 +0200

    Test access to leoilab account leo-platform-images-staging

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
+from boto3 import client
+
 print("Hello!")
+
+client = client('s3')
+for key in client.list_objects_v2(Bucket='leo-platform-images-staging', Prefix='imagine/')['Contents']:
+    print(key['Key'])
+object = client.get_object(Bucket='leo-platform-images-staging', Key='imagine/17d8889b-8698-49b7-b626-49306aa062ec')
+print(object)
